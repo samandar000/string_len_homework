60ca7d7424015e2e948bed113f93f8bf662cb76b
Samandar000 <samandarnfh.2020@gmail.com>
2022-08-18 12:18:48 +0500

    07

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,13 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    s=""
+    if len(s1)%2==1:
+        s+=s1+","
+    if len(s2)%2==1:
+        s+=s2+","
+    if len(s3)%2==1:
+        s+=s3
+    return "["+s+"]" 
+    
+    
